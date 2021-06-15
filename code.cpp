71e0f3467e272178800ca589e30f3ee34a9e7fef
Suryashankar Das <suryashankardas.2002@gmail.com>
2021-06-15 14:51:19 +0530

    use std::set
    Use std::set to maintain a sorted list of MemPoolTransactions for
    creation of block. The previous implementation was not feasable so
    currently using an algorithm that will guarantee the proper order of
    transactions but may or may not maximize the profit. It was recommended
    to not spend more than 3 days on the problem and thus this shall be the
    final change to the algorithm.

--- code.cpp
+++ code.cpp
@@ -1,19 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define MAX_WEIGHT (int)4e5
 
-pair<uint64_t, vector<string *>> dp[MAX_WEIGHT + 1];
-
-vector<string> parse_parents(string &p)
-{
-	stringstream ss(p);
-	string parent;
-	vector<string> res;
-
-	while (getline(ss, parent, ';'))
-		res.push_back(parent);
-	return res;
-}
+#define MAX_WEIGHT (int)4e6
 
 class MemPoolTransactions
 {
@@ -21,18 +9,46 @@ public:
 	string txid;
 	int fee;
 	int weight;
-	vector<string> parents;
+	int num_parents;
+	vector<string> parents, children;
 
 	MemPoolTransactions(vector<string> &params)
 	{
-		this->txid = params[0];
-		this->fee = stoi(params[1]);
-		this->weight = stoi(params[2]);
-		this->parents = parse_parents(params[3]);
+		txid = params[0];
+		fee = stoi(params[1]);
+		weight = stoi(params[2]);
+		parents = parse_parents(params[3]);
+		num_parents = parents.size();
+	}
+
+	vector<string> parse_parents(string &p)
+	{
+		stringstream ss(p);
+		string parent;
+		vector<string> res;
+
+		while (getline(ss, parent, ';'))
+			res.push_back(parent);
+		return res;
+	}
+
+	bool operator<(const MemPoolTransactions &other) const
+	{
+		if (this->num_parents == other.num_parents)
+		{
+			if (this->fee == other.fee)
+			{
+				if (this->weight == other.weight)
+					return this->txid < other.txid;
+				return this->weight > other.weight;
+			}
+			return this->fee < other.fee;
+		}
+		return this->num_parents > other.num_parents;
 	}
 };
 
-void parse_mempool_csv(vector<MemPoolTransactions> &mempool)
+void parse_mempool_csv(unordered_map<string, MemPoolTransactions *> &mempool)
 {
 	ifstream file("mempool.csv");
 	string line, col;
@@ -48,37 +64,61 @@ void parse_mempool_csv(vector<MemPoolTransactions> &mempool)
 		}
 		if (params.size() == 3)
 			params.push_back("");
-		mempool.push_back(MemPoolTransactions(params));
+		mempool[params[0]] = new MemPoolTransactions(params);
 	}
 }
 
-pair<uint64_t, vector<string *>> knapsack(vector<MemPoolTransactions> &mempool)
+class Block
 {
-	int n = mempool.size();
-	for (int i = 0; i < n; i++)
+private:
+	unordered_map<string, MemPoolTransactions *> mempool;
+	set<MemPoolTransactions> st;
+
+public:
+	vector<string> final_transactions;
+	int total_weight = 0;
+	int total_fee = 0;
+
+	Block()
 	{
-		for (int w = MAX_WEIGHT; w >= mempool[i].weight; w--)
+		parse_mempool_csv(mempool);
+		for (const pair<string, MemPoolTransactions *> &transaction : mempool)
 		{
-			if (dp[w].first < dp[w - mempool[i].weight].first + mempool[i].fee)
+			for (const string &parent : transaction.second->parents)
 			{
-				dp[w] = make_pair(dp[w - mempool[i].weight].first + mempool[i].fee,
-								  dp[w - mempool[i].weight].second);
-				// dp[w].second.push_back(&mempool[i].txid);
+				mempool[parent]->children.push_back(transaction.first);
 			}
+			st.insert(*transaction.second);
+		}
+
+		while (!st.empty())
+		{
+			if ((*st.rbegin()).weight + total_weight > MAX_WEIGHT)
+				break;
+			final_transactions.push_back((*st.rbegin()).txid);
+			total_weight += (*st.rbegin()).weight;
+			total_fee += (*st.rbegin()).fee;
+
+			for (const string &child : (*st.rbegin()).children)
+			{
+				st.erase(*mempool[child]);
+				mempool[child]->num_parents--;
+				st.insert(*mempool[child]);
+			}
+			st.erase(*(st.rbegin()));
 		}
 	}
-	return dp[MAX_WEIGHT];
-}
+};
 
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	vector<MemPoolTransactions> mempool;
-
-	parse_mempool_csv(mempool);
-	cout << knapsack(mempool).first << "\n";
+	Block block;
+	cout << "### generated block ###\n";
+	cout << "number of transactions: " << block.final_transactions.size() << "\n";
+	cout << "fees: " << block.total_fee << " weight: " << block.total_weight << "\n";
 
 	return 0;
 }
